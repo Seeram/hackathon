ceb56ef8afe3a8e27d156baf9cf0ef95ba52f493
Nazaal <18580527+mnazaal@users.noreply.github.com>
2025-05-31 18:36:18 +0300

    incorporating things to dialogue loop

--- services/agents/dialogue.py
+++ services/agents/dialogue.py
@@ -1,25 +1,34 @@
+import whisper
+import torch
+
 def process_image():
     pass
 
-def process_speech():
+def process_speech(user_input):
     # Receive speech input from user
     # Ideally perform planning
 
-    speech_input = None
-    speech_to_text = None
+    # TODO Compute embeddings, perform semantic search
+    # TODO Perform planning, or maybe planning should be done at the beginning step?
     pass
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 def process_dialogue():
     # Receive input from user, which could be speech or image
     end = False
+    model = whisper.load_model("tiny", device=device)
 
     while not end:
         # Ensure modality = text or image
-        modality = None
+        user_input = None
+        modality = None # Some function of the user input
         match modality:
             case "image":
                 pass
             case "speech":
-                pass
+                user_input_speech = model.transcribe(user_input, fp16=True,
+                                                  language="en", condition_on_previous_text=False)
+                output = process_speech(user_input_speech)
             case None:
                 pass
