a2738bc4c3ab546d4828f2eb2c740d1576f28a60
Nazaal <18580527+mnazaal@users.noreply.github.com>
2025-05-31 20:59:37 +0000

    TODOs for whats next

--- services/agents/postdialogue.py
+++ services/agents/postdialogue.py
@@ -1,11 +1,31 @@
 
-service_report = {"Report ID":None,"Ticket Number":None, "Building Address":None, ,"Date & Time of Service":None,"Technician ID":None ,"Spare Parts Replaced":None, "Service Duration (min)":None,"Technician Confidence Level (1–5)":None, "Safety Assessment (Safe/Unsafe)":None, "Additional Comments":None}
+service_report = {"Report ID":None, "Ticket Number":None, "Building Address":None, "Date & Time of Service":None,"Technician ID":None, "Spare Parts Replaced":None, "Service Duration (min)":None, "Technician Confidence Level (1–5)":None, "Safety Assessment (Safe/Unsafe)":None, "Additional Comments":None}
 
-def postprocess_service_report():
+def postprocess_service_report(report_dict):
     # Get input from user on how the task went
     # Collect any images if necessary
-    pass
+
+    # TODO Load all interactions during dialogue
+    context = None
+
+    uncertainty_threshold = 1.
+    while uncertainty < uncertainty_threshold:
+        # TODO Process loaded interactions
+
+        # TODO Fill dictionary, based on uncertainty
+
+        # TODO Compute current uncertainty over each field entry
+        pass
 
 def postprocess_validation():
     # User should validate final service report
+
+    # TODO Summarize final service report dictionary
+
+    acceptable = False
+    # TODO Receive voice input from user
+    while not acceptable:
+        # TODO Update service report dictionary
+        pass
+
     pass
