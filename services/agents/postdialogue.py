b422c13223202a6fb2dda7960baad36d8e47edb6
Nazaal <18580527+mnazaal@users.noreply.github.com>
2025-05-31 23:20:59 +0300

    cleaning

--- services/agents/postdialogue.py
+++ services/agents/postdialogue.py
@@ -1,3 +1,6 @@
+
+service_report = {"Report ID":None,"Ticket Number":None, "Building Address":None, ,"Date & Time of Service":None,"Technician ID":None ,"Spare Parts Replaced":None, "Service Duration (min)":None,"Technician Confidence Level (1–5)":None, "Safety Assessment (Safe/Unsafe)":None, "Additional Comments":None}
+
 def postprocess_service_report():
     # Get input from user on how the task went
     # Collect any images if necessary
