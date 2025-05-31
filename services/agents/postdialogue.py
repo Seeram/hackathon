af0e2cc672b790e50d388a451623eec09edd496b
Nazaal <18580527+mnazaal@users.noreply.github.com>
2025-05-31 14:49:01 +0300

    skeleton template

---
+++ services/agents/postdialogue.py
@@ -0,0 +1,8 @@
+def postprocess_service_report():
+    # Get input from user on how the task went
+    # Collect any images if necessary
+    pass
+
+def postprocess_validation():
+    # User should validate final service report
+    pass
